82d4cabd510f682667c50d4b8c9bf35253bd13e9
Alekey <alexmetal2001@gmail.com>
2025-01-06 21:52:03 -0600

    Up: Moficicación para que se cargue correctamente el logo en el template

--- Public/Templates/cabeceraFormularios_FE.php
+++ Public/Templates/cabeceraFormularios_FE.php
@@ -1,7 +1,9 @@
 <link rel="stylesheet" href="../Styles/Estilos-Formularios.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 <div class="cabecera">
-    <div class="container__logo">
-        <img src="../Img/Aca-Vendes.png" alt="" class="logo__">
+    <div class="container__btn_inicio">
+        <!-- <a href="Index.php"><div class="btn__inicio"><i class="fa-solid fa-house"></i></div></a> -->
+        <div class="btn__inicio"><img src="Public/Img/Aca-Vendes.png" alt="" style="height: 100%; width: 100%;"></div>
     </div>
 </div>
